bed16d280a87523867b9ffeb8b76b33d9cfebd49
YaCpotato <yasshisshy@gmail.com>
2019-12-24 23:21:33 +0900

    環境変数設定

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+// Initialize Firebase
+var config = {
+        apiKey: process.env.FIRE_BASE.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.FIRE_BASE.DATABASE_URL,
+        projectId: process.env.FIRE_BASE.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDERID,
+};
+firebase.initializeApp(config);
+
+/* eslint-disable no-new */
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
